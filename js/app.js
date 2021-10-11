e346b63bfb4486140b9c12e0f33381b080ff0036
Armanur Rahman <mubeenarman19@gmail.com>
2021-10-12 00:43:51 +0900

    implement smooth scrolling

--- js/app.js
+++ js/app.js
@@ -9,3 +9,28 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", () => {
     headerEl.classList.toggle("nav-open");
 });
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault;
+        const href = link.getAttribute("href");
+
+        if (href === "#") {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth",
+            });
+        }
+
+        if (href !== "#" && href.startsWith("#")) {
+            const selectedEl = document.querySelector(href);
+            selectedEl.scrollIntoView({ behavior: "smooth" });
+        }
+
+        if (link.classList.contains("main-nav-link")) {
+            headerEl.classList.toggle("nav-open");
+        }
+    });
+});
